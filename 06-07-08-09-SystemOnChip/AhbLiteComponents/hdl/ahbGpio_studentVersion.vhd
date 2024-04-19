c95c32e07f7cd261c7d6854c5135302d265c57e6
JulienChevalley <chevalleyjulien2@gmail.com>
2024-04-19 16:19:42 +0200

    LabGpioDone_StartedUART

--- 06-07-08-09-SystemOnChip/AhbLiteComponents/hdl/ahbGpio_studentVersion.vhd
+++ 06-07-08-09-SystemOnChip/AhbLiteComponents/hdl/ahbGpio_studentVersion.vhd
@@ -19,8 +19,9 @@
 --
 ARCHITECTURE studentVersion OF ahbGpio IS
 
-signal configRegister:unsigned(ioNb-1 downto 0);
-signal dataRegister:unsigned(ioNb-1 downto 0);
+signal configRegister:std_ulogic_vector(ioOut'length-1 downto 0);
+signal outputRegister, inputRegister:std_ulogic_vector(ioOut'length-1 downto 0);
+signal readyCase, writeConfig, writeOut, readIn : std_uLogic;
 
 
 BEGIN
@@ -28,32 +29,84 @@ BEGIN
 process(hClk, hReset_n)
 begin
 	if (hReset_n = '0') then
-		ioOut <= (OTHERS => '0');
-		ioEn  <= (OTHERS => '0');
-		hReady  <=	'0';	
-		hResp	  <=	'0';
-		hRData  <=	(OTHERS => '0');
+		writeOut <= '0';
+		writeConfig <= '0';
+		readIn <= '0';
+		readyCase <= '0';
 	elsif rising_edge(hClk) then
+		readIn <= '0';
+		writeOut <= '0';
+		writeConfig <= '0';
+		
+		if(readyCase = '0') then
+			readyCase <='1' ;
+		else
+			readyCase <=  readyCase;
+		end if;
 		--hAddr, hWData, hTrans, hWrite, hSel, ioIn
-		if(hSel = '1') then
-			if(hAddr = to_unsigned(0,hAddr'length)) then --write or read
-				if(hTrans=to_unsigned(2,hTrans'length) and hWrite = '1')			
-			else --only write
-				if(hTrans=to_unsigned(2,hTrans'length) and hWrite = '1') then
-					configRegister <= hWData;
-					hReady <= '1';
-				else
-					configRegister <= configRegister;
+		if hSel = '1' then
+			if hTrans = "10" then
+	
+				if(hWrite = '1') then --write 
+					if(hAddr = to_unsigned(0,hAddr'length)) then
+						writeOut <= '1';
+						--outputRegister <= hWData(ioOut'length-1 downto 0);
+					elsif (hAddr = to_unsigned(1,hAddr'length))  then
+						writeConfig <= '1';
+						--configRegister <=hWData(ioOut'length-1 downto 0);
+					end if;
+				--else --read
+					--readIn <= '1';
+					--hRData(ioOut'length-1 downto 0) <= ioIn;
 				end if;
+			else
 			end if;
 		else
-		
 		end if;
 	end if;
-	
-	
-end procees;
+end process;
 
+process(hClk, hReset_n)
+begin 
+	if (hReset_n = '0') then
+		outputRegister <= (OTHERS => '0');
+		configRegister  <= (OTHERS => '0');
+	elsif	rising_edge(hClk) then
+		--if readIn = '1' then
+		--end if;
+		
+		if writeOut = '1' then
+		outputRegister <= hWData(ioOut'length-1 downto 0);
+		end if;
+		
+		if writeConfig = '1' then
+		configRegister <=hWData(ioOut'length-1 downto 0);
+		end if;
+	end if;
+end process;
+
+
+hRData(hrData'high downto ioOut'length) <= (OTHERS => '0');
+hRData(ioOut'length-1 downto 0) <= ioIn;
+
+
+
+ioEn <=configRegister(ioEn'length-1 downto 0);
+--inputRegister(ioIn'length-1 downto 0) <= ioIn;
+ioOut <= outputRegister(ioOut'length-1 downto 0);
+
+process(readyCase)
+	begin
+		if readyCase = '0' then
+			hReady <= '0';
+		else
+			hready <='1';
+		end if;
+end process;
+
+		
+
+hResp <= '0';
 	
   -- AHB-Lite
   --hRData  <=	(OTHERS => '0');
--- 06-07-08-09-SystemOnChip/AhbLiteComponents/hdl/ahbUart_studentVersion.vhd
+++ 06-07-08-09-SystemOnChip/AhbLiteComponents/hdl/ahbUart_studentVersion.vhd
@@ -21,9 +21,78 @@
 --     bit 1: sending in progress
 --     bit 2: receiving in progress
 --
+
+
+
+
 ARCHITECTURE studentVersion OF ahbUart IS
+
+--Write Registers
+signal DataWRegister, controlRegister, periodRegister:std_ulogic_vector(txFifoDepth-1 downto 0);
+
+--Read Registers
+signal DataRRegister, statusRegister:std_ulogic_vector(txFifoDepth-1 downto 0);
+
+signal readyCase:std_ulogic;
+
+
 BEGIN
 
+process(hClk, hReset_n)
+begin 
+	if (hReset_n = '0') then
+		hRData <= (OTHERS=>'0');
+		hReady <= '0';
+		hResp <= '0';
+		TxD <= '1';
+		readyCase <= '0';
+	elsif rising_edge(hClk) then
+	
+		if(readyCase = '0') then
+			readyCase <='1' ;
+		else
+			readyCase <=  readyCase;
+		end if;
+
+		if hSel = '1' then
+			if hTrans = "10" then
+				if(hWrite = '1') then --write state
+					if(hAddr = to_unsigned(0,hAddr'length)) then
+						--writeOut <= '1';
+						DataWRegister <= hWData(txFifoDepth-1 downto 0);
+					elsif (hAddr = to_unsigned(1,hAddr'length))  then
+						--writeConfig <= '1';
+						controlRegister <=hWData(txFifoDepth-1 downto 0);
+					elsif (hAddr = to_unsigned(2, hAddr'length)) then
+						periodRegister <= hWData(txFifoDepth-1 downto 0);
+					end if;
+				else -- Read state
+					if(hAddr = to_unsigned(0,hAddr'length)) then
+						hRData(txFifoDepth-1 downto 0) <= DataRRegister;
+					elsif (hAddr = to_unsigned(1,hAddr'length)) then
+						hRData(txFifoDepth-1 downto 0) <= statusRegister;
+					end if;
+				end if;
+			else
+			end if;
+		else
+		end if;
+	end if;
+end process;
+
+
+process(readyCase)
+	begin
+		if readyCase = '0' then
+			hReady <= '0';
+		else
+			hready <='1';
+		end if;
+end process;
+
+hResp <= '0';
+
+hRData <= RxD;
   -- AHB-Lite
   hRData  <=	(OTHERS => '0');
   hReady  <=	'0';	
